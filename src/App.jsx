7641ece93617ad6b8e0ab7de2518c3611ea6e1f3
Antares Valle <antares.valle@gmail.com>
2024-10-23 15:18:36 -0700

    add HeaderNav to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 import DocumentPage from './pages/DocumentPage/DocumentPage';
+import HeaderNav from './components/HeaderNav/HeaderNav';
 
 function App() {
   const navigate = useNavigate();
 
   return (
     <div>
+      <HeaderNav/>
       <Routes>
         <Route path='/' element={<DashboardPage/>}/>
         <Route path='/dashboard' element={<DashboardPage/>}/>
