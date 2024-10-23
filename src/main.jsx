2c79467d64a6ae18cfec25a9e0c41f2606b5de04
Antares Valle <antares.valle@gmail.com>
2024-10-23 12:35:12 -0700

    add GoogleOAuthProvider to wrap the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import { Provider } from "./components/ui/provider.jsx"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+      <StrictMode>
+      <BrowserRouter>
+        <Provider>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </StrictMode>
+  </GoogleOAuthProvider> 
 )
