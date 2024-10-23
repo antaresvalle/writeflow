7e94bf63d593da89f9174ce3c1d648513fd65412
Antares Valle <antares.valle@gmail.com>
2024-10-22 19:48:52 -0700

    create pages

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import { Box, Theme } from "@chakra-ui/react"
+
+function LoginPage() {
+  return (
+    <Box>
+      <Theme >
+        <div>LoginPage</div>
+      </Theme>
+    </Box>
+  )
+}
+
+export default LoginPage
