c6fb506f79f6d2c969878e0f961bb166347c22b3
Antares Valle <antares.valle@gmail.com>
2024-10-23 12:43:42 -0700

    add GoogleLogin

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,41 @@
-import { Box, Theme } from "@chakra-ui/react"
+import { Box, Theme } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { GoogleLogin } from '@react-oauth/google';
+import  { Navigate } from 'react-router-dom'
 
 function LoginPage() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [credential, setCredential] = useState('')
+  const API_URL = import.meta.env.VITE_API_URL;
+  const token = localStorage.getItem("token");
+
+  const getToken = async (response) => {
+    const data = await axios.post(`${API_URL}/verify-token`, {
+        response
+    });
+    window.location = data.data.authUrl;
+  }
+
+  const responseMessageHandler =  (response) => {
+      const { credential } = response;
+      setCredential(credential);
+      getToken(response);
+  };
+
+  const errorMessage = (error) => {
+      console.log('Error', error)
+  }
+
+  if(token){
+    return <Navigate to="/dashboard"/>
+  } 
+
   return (
     <Box>
       <Theme >
-        <div>LoginPage</div>
+        <h2>Google Login</h2>
+        <GoogleLogin onSuccess={responseMessageHandler} onError={errorMessage} />
       </Theme>
     </Box>
   )
