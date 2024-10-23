f3074f1a0793605f9e1007593ca70cf21233a892
Antares Valle <antares.valle@gmail.com>
2024-10-22 19:49:39 -0700

    create BrowserRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Button } from '@chakra-ui/react'
+import LoginPage from './pages/LoginPage/LoginPage';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+import DocumentPage from './pages/DocumentPage/DocumentPage';
 
 function App() {
 
   return (
-    <>
-      <h1>WriteFlow</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/dashboard' element={<DashboardPage/>}/>
+        <Route path='/document/:id' element={<DocumentPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
