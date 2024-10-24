aea386925c9d5fbb422450c6c86180f9174adc6e
Antares Valle <antares.valle@gmail.com>
2024-10-23 22:52:32 -0700

    remove Chakra provider and pass BrowserRouter to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-import { Provider } from "./components/ui/provider.jsx"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from "@react-oauth/google"
-import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
-      <StrictMode>
-      <BrowserRouter>
-        <Provider>
-          <App />
-        </Provider>
-      </BrowserRouter>
+    <StrictMode>
+      <App />
     </StrictMode>
   </GoogleOAuthProvider> 
 )
