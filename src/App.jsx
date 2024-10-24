121fbc44830edd4ce7d09f7846d49c450f864be2
Antares Valle <antares.valle@gmail.com>
2024-10-23 22:53:52 -0700

    remove css import and add BrowserRouter here instead of on main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import './App.css'
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 import DocumentPage from './pages/DocumentPage/DocumentPage';
 import HeaderNav from './components/HeaderNav/HeaderNav';
 
 function App() {
-  const navigate = useNavigate();
 
   return (
-    <div>
+    <BrowserRouter>
       <HeaderNav/>
       <Routes>
         <Route path='/' element={<DashboardPage/>}/>
@@ -17,7 +15,7 @@ function App() {
         <Route path='/login' element={<LoginPage />}/> 
         <Route path='/document/:id' element={<DocumentPage/>}/>
       </Routes>
-    </div>
+    </BrowserRouter>
   )
 }
 
