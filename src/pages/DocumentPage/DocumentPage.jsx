ef8c3ed4c7fe1d574cd45fea40488d82a1846b5d
Antares Valle <antares.valle@gmail.com>
2024-10-24 14:07:00 -0700

    rename axios call to getWordCount and change it to post to send token

--- src/pages/DocumentPage/DocumentPage.jsx
+++ src/pages/DocumentPage/DocumentPage.jsx
@@ -1,15 +1,19 @@
 import './DocumentPage.scss';
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 function DocumentPage() {
   const { id } = useParams();
   const [wordCount, setWordCount] = useState(0);
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
 
-  const getDocument = async () => {
+  const getWordCount = async () => {
     try {
-        const response = await axios.get(`http://localhost:3000/documents/${id}`);
+        const response = await axios.post(`http://localhost:3000/documents/${id}`, {
+          token: token
+        });
         setWordCount(response.data.wordCount)
     } catch(error){
       console.error('Error getting the word count', error)
@@ -17,7 +21,10 @@ function DocumentPage() {
   };
 
   useEffect(() => {
-    const interval = setInterval(() => getDocument(), 1000);
+    if(!token) {
+      navigate('/login');
+    }
+    const interval = setInterval(() => getWordCount(), 1000);
     return () => clearInterval(interval)
   }, []);
     
