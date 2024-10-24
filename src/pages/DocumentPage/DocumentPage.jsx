8e95e1b90dcbb76ee5e7e126d7b127982cf6c0ca
Antares Valle <antares.valle@gmail.com>
2024-10-24 09:56:39 -0700

    add setInterval to get wordCount every second

--- src/pages/DocumentPage/DocumentPage.jsx
+++ src/pages/DocumentPage/DocumentPage.jsx
@@ -4,20 +4,22 @@ import { Link, useParams } from "react-router-dom";
 import axios from 'axios';
 
 function DocumentPage() {
-    const { id } = useParams();
-    const [wordCount, setWordCount] = useState(0);
+  const { id } = useParams();
+  const [wordCount, setWordCount] = useState(0);
 
-    useEffect(()=>{
-        const getDocument = async () => {
-            try {
-                const response = await axios.get(`http://localhost:3000/documents/${id}`);
-                setWordCount(response.data.wordCount)
-            } catch(error){
-              console.error('Error getting the word count', error)
-            }
-        }
-        getDocument();
-    }, [])
+  const getDocument = async () => {
+    try {
+        const response = await axios.get(`http://localhost:3000/documents/${id}`);
+        setWordCount(response.data.wordCount)
+    } catch(error){
+      console.error('Error getting the word count', error)
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getDocument(), 1000);
+    return () => clearInterval(interval)
+  }, []);
     
   return (
     <main className="document">
